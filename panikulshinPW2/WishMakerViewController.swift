c6090adac2350db5f112a72a31be099320265d4a
AKSUMRUS <shimorhershow@gmail.com>
2023-10-06 03:31:52 +0300

    upd: deleted some excess code

--- panikulshinPW2/WishMakerViewController.swift
+++ panikulshinPW2/WishMakerViewController.swift
@@ -1,16 +1,5 @@
 import UIKit
 
-extension UIColor {
-   convenience init(rgb: Int) {
-       self.init(
-           red: CGFloat((rgb >> 16) & 0xFF) / 255.0,
-           green: CGFloat((rgb >> 8) & 0xFF) / 255.0,
-           blue: CGFloat(rgb & 0xFF) / 255.0,
-           alpha: 1
-       )
-   }
-}
-
 final class WishMakerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
