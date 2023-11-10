c923d541c7b0ddaa0d0a3c6b48c86db33a361824
AKSUMRUS <shimorhershow@gmail.com>
2023-11-11 00:46:24 +0300

    HW-3: 3 points

--- panikulshinPW2/UI/WishStoringViewController.swift
+++ panikulshinPW2/UI/WishStoringViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 final class WishStoringViewController: UIViewController {
     
+    private let table: UITableView = UITableView(frame: .zero)
+    
     enum Constants {
         static let lineWidth: CGFloat = 200
         static let lineHeight: CGFloat = 3.4
         static let lineTop: CGFloat = 10
         static let lineRadius: CGFloat = 5
+        
+        static let tableOffset: CGFloat = 20
+        static let tableRadius: CGFloat = 15
     }
     
     override func viewDidLoad() {
@@ -24,6 +29,7 @@ final class WishStoringViewController: UIViewController {
         view.backgroundColor = .blue
         
         configureLine()
+        configureTable()
     }
     
     private func configureLine() {
@@ -37,5 +43,30 @@ final class WishStoringViewController: UIViewController {
         
         line.pinCenterX(to: view)
         line.pinTop(to: view.topAnchor, Constants.lineTop)
+    }
+    
+    private func configureTable() {
+        view.addSubview(table)
+        
+        table.backgroundColor = .red
+        table.dataSource = self
+        table.separatorStyle = .none
+        table.layer.cornerRadius = Constants.tableRadius
+        
+        table.setWidth(100) // just for test
+        table.setHeight(100) // just for test
+        table.pinCenterX(to: view)
+        table.pinTop(to: view.topAnchor, Constants.tableOffset)
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension WishStoringViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
